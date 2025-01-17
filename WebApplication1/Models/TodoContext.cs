ce12f9169da83a62bdfe8d843dfe4877ea066127
João Vitor Rodrigues Brito <52049527+JoaoVitor2344@users.noreply.github.com>
2025-01-17 17:17:21 -0300

    feat:iniciado

---
+++ WebApplication1/Models/TodoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class TodoContext(DbContextOptions<TodoContext> options) : DbContext(options)
+    {
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
